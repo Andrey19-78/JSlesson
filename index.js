62bad65f1e7ebfc24415b7c20e2dbda2ab908444
Andrey19-78 <113016337+Andrey19-78@users.noreply.github.com>
2022-09-12 21:12:02 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+function numBer(a){
+    n = a % 10;
+    b = a / 10|0;
+    n1 = b % 10;
+    n2 = b / 10|0;
+    b = n * 100 + n1 * 10 + n2
+{
+    return b
+}
+}
+console.log(numBer(617))
+
+function sumCub(a,b,c){
+    n = (a + b + c) ** 3
+    {
+        return n  
+    }
+}
+console.log(sumCub(1,2,1))
